b28caa60ff60a56d69cb28055b251e206be3f375
ilkay22 <156299462+ilkay22@users.noreply.github.com>
2024-05-25 13:40:05 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="icon" type="image/x-icon" href="rfid.ico">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+   
+</head>
+<body>
+    <div class="nav">
+        <div class="logo">
+            <p>Ilkay</p>
+        </div>
+        
+            <button type="submit" class="btn" name="Log uit" onclick="document.location='login.php'">Logout</button>   
+    </div>
+    <main>
+
+      <div class="main-box top">
+        <div class="top">
+            <div class="box">
+                <p>Hallo <b>Gebruiker</b>, Welkom op mijn website</p>
+            </div>
+            <div class="box">
+                <p>Dit is een <b>Test site</b>, Met een private acces key.</p>
+            </div>
+        </div>
+      </div>
+
+    </main>
+</body>
+</html>
